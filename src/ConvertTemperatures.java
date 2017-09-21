462b95783c358b0468d33d0ff4070616cb3a2d2d
LiselotteEL <lise1066@edu.easj.dk>
2017-09-21 14:22:30 +0200

    .

---
+++ src/ConvertTemperatures.java
@@ -0,0 +1,13 @@
+public class ConvertTemperatures {
+    public static void main(String[] args) {
+
+        double celcius;
+
+        double fahrenheit = 100;
+        
+        celcius = (5.0 / 9) * (fahrenheit - 32);
+
+        System.out.println(fahrenheit + " fahrenheit = " + celcius + " celsius ");
+
+    }
+}
